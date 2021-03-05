cf0f3a6eace142cc018a41e6468c60d60cb77071
dawright22 <53329137+dawright22@users.noreply.github.com>
2021-03-05 16:47:53 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,3 +17,15 @@ variable "services" {
   }))
   description = "Services and query alert thresholds"
 }
+
+variable "datadog_api_key" {
+  type  = string
+  }
+
+variable "datadog_app_key" {
+  type = string
+  }
+
+variable "datadog_api_url" {
+  type = string
+}
